33ea1c136fda61a6cdaff51191a032cca7023ecf
xxradar <xxradar@radarhack.com>
2024-05-28 12:11:59 +0000

    add ML inference pipeline

--- logging_es/elastic_connector.py
+++ logging_es/elastic_connector.py
@@ -5,10 +5,10 @@ from datetime import datetime
 
 ES_CLIENT = Elasticsearch(
   "https://b2df3cb9e1674220b93a11585c454084.westeurope.azure.elastic-cloud.com:443",
-  api_key = os.environ.get('ES_API_KEY')
+  api_key = os.environ.get('ES_API_KEY'),
 )
 
 def log_to_elasticsearch(log_data: dict):
     print(log_data)
-    res = ES_CLIENT.index(index="demo-ai-search", body=log_data)
+    res = ES_CLIENT.index(index="demo-ai-search", pipeline="ml-inference-demo-ai-search", body=log_data)
     print(f"Logged to Elasticsearch with result: {res['result']}")
