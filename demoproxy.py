acef02c826c5c34270597207bac7b6bd3d1c4aba
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2024-05-28 09:53:28 +0200

    Update demoproxy.py

--- demoproxy.py
+++ demoproxy.py
@@ -1,27 +1,42 @@
 import os
+import json
 from flask import Flask, request, jsonify
 from langchain.agents import AgentExecutor, create_react_agent
-from langchain_openai import ChatOpenAI 
+from langchain_openai import ChatOpenAI
 from langchain_experimental.tools import PythonREPLTool
 from elasticapm.contrib.flask import ElasticAPM
-from elasticsearch import Elasticsearch
-from logging_es.elastic_connector import log_to_elasticsearch
+from logging.handlers import RotatingFileHandler
 from langchain_core.prompts import PromptTemplate
 from datetime import datetime
+import logging
+from logging_es.elastic_connector import log_to_elasticsearch
 
-
-import os, json
-
+# Initialize Flask application
 app = Flask(__name__)
 
+# Configure Elastic APM
 app.config['ELASTIC_APM'] = {
-    'SERVICE_NAME': 'demo-ai-search" ',
-    'SECRET_TOKEN': '',
-    'SERVER_URL': 'http://localhost:8200'
+    'SERVICE_NAME': 'demo-ai-search',
+    'SECRET_TOKEN': '3GwyKEVn8I8YTMnmqw',
+    'SERVER_URL': 'https://4768d2bd240c46baa56babc5b7bdced9.apm.westeurope.azure.elastic-cloud.com:443',
+    'ENVIRONMENT': 'my-environment',
 }
-
 apm = ElasticAPM(app)
 
+# Set up logging
+def setup_logging(app):
+    # Set up a specific logger with our desired output level
+    handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+    app.logger.setLevel(logging.DEBUG)
+    app.logger.debug("Logging setup complete")
+
+setup_logging(app)
+
+# Prompting
 template = '''Answer the following questions as best you can. You have access to the following tools:
 
 {tools}
@@ -44,84 +59,59 @@ Thought:{agent_scratchpad}'''
 
 prompt = PromptTemplate.from_template(template)
 
-
 @app.route("/chat/completions", methods=["POST"])
 def openai_proxy():
-    # Extract OpenAI client.chat.completions parameters
     data = request.get_json()
     messages = data.get("messages", [])
     response_content = ""
 
     # Langchain agent to calculate results
     tools = [PythonREPLTool()]
-    #llm = ChatOpenAI(base_url="http://192.168.0.140:4000")
     llm = ChatOpenAI()
 
-    # agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True, stream=True)
     agent = create_react_agent(llm, tools, prompt)
     agent_executor = AgentExecutor(agent=agent, tools=tools)
 
     # Run the agent
-    response = agent_executor.invoke(
-    {
-        "input": messages,
-        "chat_history": ""
-    })
-
+    response = agent_executor.invoke({"input": messages, "chat_history": ""})
     response_content = response["output"]
 
-    print("------DEBUG1--------")
-    print(response)
-    print(response_content)
-    print("------DEBUG1--------")
-
     # Prepare log data for ES
-    now = datetime.now()
-    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
     log_data = {
-       "request": {"messages": messages[-1]},
-       "response": response_content, 
-       "timestamp": date_time
-        }
-    log = json.dumps(log_data)
+       "request": messages[-1] if messages else "",
+       "response": response_content,
+       "@timestamp": datetime.now().isoformat()
+    }
 
-    print("------DEBUG2--------")
-    print(log)
-    print("------DEBUG2--------")
-
-    
-    apm.capture_message(log_data)
-    log_to_elasticsearch(log)
+    # Log processing request
+    app.logger.info(log_data)
+    apm.capture_message(json.dumps(log_data))
+    log_to_elasticsearch(log_data)
 
     # Calculate token usage
     prompt_tokens = sum(len(message) for message in messages)
-    completion_tokens = len(response)
+    completion_tokens = len(response_content)
     total_tokens = prompt_tokens + completion_tokens
 
-    # Based on the results, create a OpenAI client.chat.completions response
-    return jsonify(
-        {
-            "choices": [
-                {
-                    "finish_reason": "stop",
-                    "index": 0,
-                    "message": {
-                    "content": [response],
+    # Based on the results, create an OpenAI client.chat.completions response
+    return jsonify({
+        "choices": [
+            {
+                "finish_reason": "stop",
+                "index": 0,
+                "message": {
+                    "content": response_content,
                     "role": "assistant"
-                },
                 }
-            ],
-            "usage": {
-                "prompt_tokens": prompt_tokens,
-                "completion_tokens": completion_tokens,
-                "total_tokens":  total_tokens,
-            },
+            }
+        ],
+        "usage": {
+            "prompt_tokens": prompt_tokens,
+            "completion_tokens": completion_tokens,
+            "total_tokens": total_tokens,
         }
-    )
-
-
-
+    })
 
 # Start the development server
 if __name__ == "__main__":
-    app.run( port=5000)
+    app.run(port=5000)
