15d968d01f4701c287b6b799438edf96ace440f7
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2024-06-02 14:06:32 +0200

    Add files via upload

---
+++ otel_research/testopanai.py
@@ -0,0 +1,46 @@
+import openai
+from flask import Flask
+import monitor  # Import the module
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+import urllib
+import os
+from opentelemetry import trace
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+
+# OpenTelemetry setup up code here, feel free to replace the “your-service-name” attribute here.
+resource = Resource(attributes={
+    SERVICE_NAME: "openai_otel"
+})
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(OTLPSpanExporter(endpoint=os.getenv('OTEL_EXPORTER_OTLP_ENDPOINT'),
+        headers="Authorization=Bearer%20"+os.getenv('OTEL_EXPORTER_OTLP_AUTH_HEADER')))
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+RequestsInstrumentor().instrument()
+
+
+
+# Initialize Flask app and instrument it
+
+app = Flask(__name__)
+# Set OpenAI API key
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+@app.route("/completion")
+@tracer.start_as_current_span("do_work")
+def completion():
+    response = openai.Completion.create(
+        model="davinci-002",
+        prompt="Why is Elastic an amazing observability tool?",
+        max_tokens=300,
+        temperature=0
+    )
+    return response.choices[0].text.strip()
+
+if __name__ == "__main__":
+    app.run()
---
+++ otel_research/testopenai2.py
@@ -0,0 +1,55 @@
+import openai
+from flask import Flask, jsonify
+import monitor2  # Import the module
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+import urllib
+import os
+import json
+from opentelemetry import trace
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+
+# OpenTelemetry setup up code here, feel free to replace the “your-service-name” attribute here.
+resource = Resource(attributes={
+    SERVICE_NAME: "openai_otel"
+})
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(OTLPSpanExporter(endpoint=os.getenv('OTEL_EXPORTER_OTLP_ENDPOINT'),
+        headers="Authorization=Bearer%20"+os.getenv('OTEL_EXPORTER_OTLP_AUTH_HEADER')))
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+RequestsInstrumentor().instrument()
+
+
+
+# Initialize Flask app and instrument it
+
+app = Flask(__name__)
+
+# Set OpenAI API key
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+@app.route("/completion")
+@tracer.start_as_current_span("do_work")
+def completion():
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-4-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": "Tell me a joke."}
+            ],
+            max_tokens=250,
+            temperature=1,
+        )
+        joke = response.choices[0].message['content'].strip()
+        return jsonify(joke=joke)
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+if __name__ == "__main__":
+    app.run()
