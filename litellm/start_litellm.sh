08f141d4d3c2e2848ec26b4968ce55dd417992ef
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2024-06-03 10:08:17 +0200

    Update start_litellm.sh

--- litellm/start_litellm.sh
+++ litellm/start_litellm.sh
@@ -1,1 +1,6 @@
-
+docker run  \
+    -v $(pwd)/config.yaml:/app/config.yaml \
+    -e OPENAI_API_KEY=$OPENAI_API_KEY \
+    -p 127.0.0.1:4000:4000 \
+    -d  \
+    ghcr.io/berriai/litellm:main-latest --config /app/config.yaml --detailed_debug
